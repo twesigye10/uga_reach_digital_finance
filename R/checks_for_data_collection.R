baf51ef3786175eee9e67583081c286d9a07a1ae
Anthony Twesigye <twesigye10@gmail.com>
2021-08-17 17:20:02 +0300

    preliminary logical checks

---
+++ R/checks_for_data_collection.R
@@ -0,0 +1,30 @@
+# logical checks
+
+# Anyone who selected "ugandan" and previously answered community_type = refugee, should be checked.
+
+# Anyone who selected host for "type of community" and answers "refugee ID" or "beneficiary ID" should be checked.
+
+# If respondents have selected a language but have NOT selected the same language that they previously selected for their main language, we need to check the survye.
+
+
+# If respondent has selected "none" in addition to another option, the survey needs to be checked.
+
+# If previously selected "0" in response to "how many mobile phone numbers do you have" the survye needs to be checked.
+
+
+# If they previously selected "yes" to having mobile internet coverage (Q56) and now replied "no", the survey needs to be checked.
+ 
+# Do you currently use mobile internet (social media, apps, and websites like WhatsApp, Messenger, Facebook, <other locally relevant>, etc)?
+ 
+# If respondents who previouslysaid they DO NOT have access to a feature phone or smart phone are now selecting uses for their phones that can only be done online (e.g. social media, access to information online etc.), survey needs to be checked
+
+# If respondents who previouslysaid they DO NOT have access to a feature phone or smart phone are now selecting reasons for using their phones that can only be done online (e.g. online education; looking for specific information etc.), survey needs to be checked
+ 
+
+# If in previous qn "why do you want to have  a mobile money account?" they answered "it is safer than keeping cash at home" and they now asnwered "the system is not safe i am concerned that my money will disappear", survey needs to be checked
+
+ 
+# if in previous question 'why do you want to have a bank account? ' is "Yes, it will allow me to securely store my money" and they now answered "the system isnt safe i am concerned that my money will disappear", survey needs to be checked
+
+
+# if in previous question 'Why do you want to have a pre-paid or smart card?' answered "it will allow me to securely store my money" and they now chose "the system is not safe i am concerned that my money will disappear", check survey
