b0c459a1f59f50fef5261d2db8507e0fe4d7d4da
Anthony Twesigye <twesigye10@gmail.com>
2021-08-18 18:11:39 +0300

    check: reason_want_mm_acc

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -199,7 +199,19 @@ df_c_phone_use <- df_tool_data %>%
   rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
 
 # If in previous qn "why do you want to have  a mobile money account?" they answered "it is safer than keeping cash at home" and they now asnwered "the system is not safe i am concerned that my money will disappear", survey needs to be checked
-
+# reason_want_mm_acc/safer_than_home == 1 and reason_not_open_mm_acc/unsafe_system
+df_c_reason_want_mm_acc <- df_tool_data %>% 
+  filter("reason_want_mm_acc/safer_than_home" == 1, "reason_not_open_mm_acc/unsafe_system" == 1) %>% 
+  mutate(i.check.identified_issue = "un_expected_response",
+         i.check.type = NA,
+         i.check.name = "reason_want_mm_acc",
+         i.check.current_value = NA,
+         i.check.value = NA,
+         i.check.checked_by = "Mathias",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = NA) %>% 
+  select(starts_with("i.check"))%>% 
+  rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
 
 # if in previous question 'why do you want to have a bank account? ' is "Yes, it will allow me to securely store my money" and they now answered "the system isnt safe i am concerned that my money will disappear", survey needs to be checked
 
