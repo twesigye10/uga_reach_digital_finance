63235678f82b586b1a97dc0c0369a16c1edc5724
Anthony Twesigye <twesigye10@gmail.com>
2021-08-18 06:47:14 +0300

    check the time between surveys

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -19,48 +19,64 @@ min_time_of_survey <- 40
 max_time_of_survey <- 120
 
 df_c_survey_time <-  df_tool_data %>% 
-  mutate(
-    int.survey_time_interval = difftime(end,start, units = "mins"),
-    int.survey_time_interval = round(int.survey_time_interval,2),
-    i.check.identified_issue = case_when(
-      int.survey_time_interval < min_time_of_survey ~ "less_survey_time",
-      int.survey_time_interval > max_time_of_survey ~ "more_survey_time",
-      TRUE ~ "normal_survey_time" ),
-    i.check.type = NA,
-    i.check.name = NA,
-    i.check.value = NA,
-    i.check.checked_by = "Mathias",
-    i.check.checked_date = as_date(today()),
-    i.check.comment = NA
-  )%>% 
+  mutate(int.survey_time_interval = difftime(end,start, units = "mins"),
+         int.survey_time_interval = round(int.survey_time_interval,2),
+         i.check.identified_issue = case_when(
+           int.survey_time_interval < min_time_of_survey ~ "less_survey_time",
+           int.survey_time_interval > max_time_of_survey ~ "more_survey_time",
+           TRUE ~ "normal_survey_time" ),
+         i.check.type = NA,
+         i.check.name = NA,
+         i.check.value = NA,
+         i.check.checked_by = "Mathias",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = NA)%>% 
   filter(i.check.identified_issue %in% c("less_survey_time", "more_survey_time")) %>% 
   select(starts_with("i.check"))%>% 
   rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
 
+
+# check the time between surveys ------------------------------------------
+
+df_c_time_btn_survey <- df_tool_data %>%
+  group_by( today, enumerator_id) %>%
+  arrange(start, .by_group = TRUE) %>% 
+  mutate(int.t_between_survey = (start - lag(end, default=first(start))),
+         int.time_between_survey = make_difftime(int.t_between_survey, units = "mins"),
+         int.time_between_survey = round(int.time_between_survey,2)) %>%
+  filter(int.time_between_survey !=0 & int.time_between_survey < 5) %>%
+  mutate(i.check.identified_issue = "less_time_btn_surveys",
+         i.check.type = NA,
+         i.check.name = NA,
+         i.check.value = NA,
+         i.check.checked_by = "Mathias",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = NA )
+
 # Anyone who selected "ugandan" and previously answered community_type = refugee, should be checked.
 df_c_nationality <- df_tool_data %>% 
-# Anyone who selected host for "type of community" and answers "refugee ID" or "beneficiary ID" should be checked.
+  # Anyone who selected host for "type of community" and answers "refugee ID" or "beneficiary ID" should be checked.
+  
+  # If respondents have selected a language but have NOT selected the same language that they previously selected for their main language, we need to check the survye.
+  
+  
+  # If respondent has selected "none" in addition to another option, the survey needs to be checked.
+  
+  # If previously selected "0" in response to "how many mobile phone numbers do you have" the survye needs to be checked.
+  
+  
+  # If they previously selected "yes" to having mobile internet coverage (Q56) and now replied "no", the survey needs to be checked.
 
-# If respondents have selected a language but have NOT selected the same language that they previously selected for their main language, we need to check the survye.
-
-
-# If respondent has selected "none" in addition to another option, the survey needs to be checked.
-
-# If previously selected "0" in response to "how many mobile phone numbers do you have" the survye needs to be checked.
-
-
-# If they previously selected "yes" to having mobile internet coverage (Q56) and now replied "no", the survey needs to be checked.
- 
 # Do you currently use mobile internet (social media, apps, and websites like WhatsApp, Messenger, Facebook, <other locally relevant>, etc)?
- 
+
 # If respondents who previously said they DO NOT have access to a feature phone or smart phone are now selecting uses for their phones that can only be done online (e.g. social media, access to information online etc.), survey needs to be checked
 
 # If respondents who previously said they DO NOT have access to a feature phone or smart phone are now selecting reasons for using their phones that can only be done online (e.g. online education; looking for specific information etc.), survey needs to be checked
- 
+
 
 # If in previous qn "why do you want to have  a mobile money account?" they answered "it is safer than keeping cash at home" and they now asnwered "the system is not safe i am concerned that my money will disappear", survey needs to be checked
 
- 
+
 # if in previous question 'why do you want to have a bank account? ' is "Yes, it will allow me to securely store my money" and they now answered "the system isnt safe i am concerned that my money will disappear", survey needs to be checked
 
 
