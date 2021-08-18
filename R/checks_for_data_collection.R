d57bb4fa6851f338eb9bda2bd10ca1c33c07b474
Anthony Twesigye <twesigye10@gmail.com>
2021-08-18 07:26:50 +0300

    check for id_type

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -66,7 +66,7 @@ df_c_time_btn_survey <- df_tool_data %>%
 # Anyone who selected "ugandan" and previously answered community_type = refugee, should be checked.
 df_c_nationality <- df_tool_data %>% 
   filter(status == "refugee", nationality == "ugandan") %>% 
-  mutate(i.check.identified_issue = "value_outside_limits",
+  mutate(i.check.identified_issue = "un_expected_response",
          i.check.type = NA,
          i.check.name = "nationality",
          i.check.value = nationality,
@@ -76,18 +76,28 @@ df_c_nationality <- df_tool_data %>%
   select(starts_with("i.check"))%>% 
   rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
 
+# Anyone who selected host for "type of community" and answers "refugee ID" or "beneficiary ID" should be checked.
+df_c_id_type <- df_tool_data %>% 
+  filter(status == "host_community", id_type %in% c("unhcr_refugee_id", "ug_refugee_id", "benef_id_not_unhcr")) %>% 
+  mutate(i.check.identified_issue = "un_expected_response",
+         i.check.type = NA,
+         i.check.name = "id_type",
+         i.check.value = id_type,
+         i.check.checked_by = "Mathias",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = NA) %>% 
+  select(starts_with("i.check"))%>% 
+  rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
 
-  # Anyone who selected host for "type of community" and answers "refugee ID" or "beneficiary ID" should be checked.
-  
-  # If respondents have selected a language but have NOT selected the same language that they previously selected for their main language, we need to check the survye.
-  
-  
-  # If respondent has selected "none" in addition to another option, the survey needs to be checked.
-  
-  # If previously selected "0" in response to "how many mobile phone numbers do you have" the survye needs to be checked.
-  
-  
-  # If they previously selected "yes" to having mobile internet coverage (Q56) and now replied "no", the survey needs to be checked.
+# If respondents have selected a language but have NOT selected the same language that they previously selected for their main language, we need to check the survye.
+
+
+# If respondent has selected "none" in addition to another option, the survey needs to be checked.
+
+# If previously selected "0" in response to "how many mobile phone numbers do you have" the survye needs to be checked.
+
+
+# If they previously selected "yes" to having mobile internet coverage (Q56) and now replied "no", the survey needs to be checked.
 
 # Do you currently use mobile internet (social media, apps, and websites like WhatsApp, Messenger, Facebook, <other locally relevant>, etc)?
 
