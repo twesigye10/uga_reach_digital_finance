6b7b85f537a8467cf4438def8d16bcefefb39642
Anthony Twesigye <twesigye10@gmail.com>
2021-08-24 14:19:55 +0300

    rename parent qn to name

--- R/checks_for_other_responses.R
+++ R/checks_for_other_responses.R
@@ -34,11 +34,11 @@ df_grouped_choices <- df_choices %>%
 # extract parent question and join survey for extracting list_name
 df_data_parent_qns <- df_data_arranged %>% 
   mutate(
-    parent_qn = str_replace_all(name, "/.*", ""),
-    parent_qn = str_replace_all(parent_qn, "_other", "")
+    parent_qn = str_replace_all(other_name, "_other", "")
   ) %>% 
   left_join(df_survey %>% select(name, type), by = c("parent_qn"="name")) %>% 
-  separate(col = type, into = c("select_type", "list_name"), sep =" ", remove = TRUE, extra = "drop" )
+  separate(col = type, into = c("select_type", "list_name"), sep =" ", remove = TRUE, extra = "drop" ) %>% 
+  rename(name = parent_qn)
 
 # join other responses with choice options based on list_name
 
