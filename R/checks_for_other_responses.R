cb7f5c422bc605c872114dac5f120f8f62f0b9ae
Anthony Twesigye <twesigye10@gmail.com>
2021-08-24 18:43:36 +0300

    refactoring others_checks into a function

--- R/checks_for_other_responses.R
+++ R/checks_for_other_responses.R
@@ -8,11 +8,12 @@ df_tool_data <- readxl::read_excel("inputs/UGA2103_Financial_Service_Providers_A
 df_survey <- readxl::read_excel("inputs/UGA2103_Digital_Finace_HH_Tool_June2021.xlsx", sheet = "survey")
 df_choices <- readxl::read_excel("inputs/UGA2103_Digital_Finace_HH_Tool_June2021.xlsx", sheet = "choices")
 
-extract_other_data <- function(input_tool_data) {
+extract_other_data <- function(input_tool_data, input_survey, input_choices) {
   # get questions with other
   others_colnames <-  input_tool_data %>% 
     select(ends_with("_other"), -contains("/")) %>% 
     colnames()
+  
   # data.frame for holding _other response data
   df_other_response_data <- data.frame()
   
@@ -23,57 +24,58 @@ extract_other_data <- function(input_tool_data) {
       mutate( other_name = cln, value = NA)
     df_other_response_data <- rbind(df_other_response_data, df_filtered_data)
   }
+  
   # arrange the data
-  df_other_response_data %>% 
+  df_data_arranged <- df_other_response_data %>% 
     arrange(start_date, uuid)
+  
+  # get choices to add to the _other responses extracted
+  df_grouped_choices <- input_choices %>% 
+    group_by(list_name) %>% 
+    summarise(choice_options = paste(name, collapse = " : ")) %>% 
+    arrange(list_name)
+  
+  # extract parent question and join survey for extracting list_name
+  df_data_parent_qns <- df_data_arranged %>% 
+    mutate(
+      parent_qn = str_replace_all(other_name, "_other", "")
+    ) %>% 
+    left_join(input_survey %>% select(name, type), by = c("parent_qn"="name")) %>% 
+    separate(col = type, into = c("select_type", "list_name"), sep =" ", remove = TRUE, extra = "drop" ) %>% 
+    rename(name = parent_qn)
+  
+  # join other responses with choice options based on list_name
+  df_join_other_response_with_choices <- df_data_parent_qns %>% 
+    left_join(df_grouped_choices, by = "list_name") %>% 
+    mutate(current_value = "other", issue_id = "other_checks")
+  
+  # care for select_one and select_multiple (change_response, add_option, remove_option)
+  output <- list()
+  # select_one checks
+  output$select_one <- df_join_other_response_with_choices %>% 
+    filter(str_detect(select_type, c("select_one|select one"))) %>% 
+    mutate(type = "change_response")
+  
+  # select_multiple checks
+  select_mu_data <- df_join_other_response_with_choices %>% 
+    filter(str_detect(select_type, c("select_multiple|select multiple")))
+  
+  select_mu_add_option <- select_mu_data %>% 
+    mutate(type = "add_option")
+  select_mu_remove_option <- select_mu_data %>% 
+    mutate(type = "remove_option")
+  
+  output$select_multiple <- bind_rows(select_mu_add_option, select_mu_remove_option) %>% 
+    arrange(uuid, start_date, enumerator_id, name)
+  
+  # merge other checks
+  merged_other_checks <- bind_rows(output) %>% 
+    select(uuid, start_date, enumerator_id, district_name, point_number, type, name, current_value, value, issue_id, other_text)
 }
 
 # others data
-df_data_arranged <- extract_other_data(input_tool_data = df_tool_data)
 
-# get choices to add to the _other responses extracted
-df_grouped_choices <- df_choices %>% 
-  group_by(list_name) %>% 
-  summarise(choice_options = paste(name, collapse = " : ")) %>% 
-  arrange(list_name)
-
-# extract parent question and join survey for extracting list_name
-df_data_parent_qns <- df_data_arranged %>% 
-  mutate(
-    parent_qn = str_replace_all(other_name, "_other", "")
-  ) %>% 
-  left_join(df_survey %>% select(name, type), by = c("parent_qn"="name")) %>% 
-  separate(col = type, into = c("select_type", "list_name"), sep =" ", remove = TRUE, extra = "drop" ) %>% 
-  rename(name = parent_qn)
-
-# join other responses with choice options based on list_name
-
-df_join_other_response_with_choices <- df_data_parent_qns %>% 
-  left_join(df_grouped_choices, by = "list_name") %>% 
-  mutate(current_value = "other", issue_id = "other_checks")
-
-# care for select_one and select_multiple (change_response, add_option, remove_option)
-output <- list()
-# select_one checks
-output$select_one <- df_join_other_response_with_choices %>% 
-  filter(str_detect(select_type, c("select_one|select one"))) %>% 
-  mutate(type = "change_response")
-
-# select_multiple checks
-select_mu_data <- df_join_other_response_with_choices %>% 
-  filter(str_detect(select_type, c("select_multiple|select multiple")))
-
-select_mu_add_option <- select_mu_data %>% 
-  mutate(type = "add_option")
-select_mu_remove_option <- select_mu_data %>% 
-  mutate(type = "remove_option")
-
-output$select_multiple <- bind_rows(select_mu_add_option, select_mu_remove_option) %>% 
-  arrange(uuid, start_date, enumerator_id, name)
-
-# merge other checks
-merged_other_checks <- bind_rows(output) %>% 
-  select(uuid, start_date, enumerator_id, district_name, point_number, type, name, current_value, value, issue_id, other_text)
+df_others_data <- extract_other_data(input_tool_data = df_tool_data, input_survey = df_survey, input_choices = df_choices)
 
 # output the resulting data frame
-write_csv(x = merged_other_checks, file = paste0("outputs/others_responses_",as_date(today()),"_", hour(now()) ,".csv"), na = "")
+write_csv(x = df_others_data, file = paste0("outputs/others_responses_",as_date(today()),"_", hour(now()) ,".csv"), na = "")
