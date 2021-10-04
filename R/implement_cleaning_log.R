28c7238f8ed2a002249b37e65dc56432d600246b
Anthony Twesigye <twesigye10@gmail.com>
2021-10-04 14:53:23 +0300

    removing unwanted column

--- R/implement_cleaning_log.R
+++ R/implement_cleaning_log.R
@@ -10,7 +10,8 @@ df_cleaning_log <- read_csv("inputs/combined_logic_spatial_and_others_checks.csv
   mutate(sheet = NA, index = NA, relevant = NA) %>% 
   select(uuid, type, name, value, issue_id, sheet, index, relevant, issue)
 
-df_raw_data <- readxl::read_excel("inputs/UGA2103_Financial_Service_Providers_Assessment_HH_Tool_June2021.xlsx")
+df_raw_data <- readxl::read_excel("inputs/UGA2103_Financial_Service_Providers_Assessment_HH_Tool_June2021.xlsx") %>% 
+  select(-`id_type_refugee/school_ID`)
 
 df_survey <- readxl::read_excel("inputs/UGA2103_Digital_Finace_HH_Tool_June2021.xlsx", sheet = "survey") 
 df_choices <- readxl::read_excel("inputs/UGA2103_Digital_Finace_HH_Tool_June2021.xlsx", sheet = "choices") 
