2cc0547bef0c0e19b15a17e9e6e78757a382d740
Anthony Twesigye <twesigye10@gmail.com>
2021-10-04 09:23:32 +0300

    get new choices to add to choices sheet

--- R/implement_cleaning_log.R
+++ R/implement_cleaning_log.R
@@ -11,3 +11,22 @@ df_raw_data <- readxl::read_excel("inputs/UGA2103_Financial_Service_Providers_As
 
 df_survey <- readxl::read_excel("inputs/UGA2103_Digital_Finace_HH_Tool_June2021.xlsx", sheet = "survey") 
 df_choices <- readxl::read_excel("inputs/UGA2103_Digital_Finace_HH_Tool_June2021.xlsx", sheet = "choices") 
+
+# find all new choices to add to choices sheet ----------------------------
+
+# gather choice options based on unique choices list
+df_grouped_choices<- df_choices %>% 
+  group_by(list_name) %>% 
+  summarise(choice_options = paste(name, collapse = " : "))
+# get new name and ad_option pairs to add to the choices sheet
+new_vars <- df_cleaning_log %>% 
+  filter(type %in% c("change_response", "add_option")) %>% 
+  left_join(df_survey, by = "name") %>% 
+  filter(str_detect(string = type.y, pattern = "select_one|select one|select_multiple|select multiple")) %>% 
+  separate(col = type.y, into = c("select_type", "list_name"), sep =" ", remove = TRUE, extra = "drop") %>% 
+  left_join(df_grouped_choices, by = "list_name") %>%
+  filter(!str_detect(string = choice_options, pattern = value ) ) %>%
+  rename(choice = value ) %>%
+  select(name, choice) %>%
+  distinct() %>% # to make sure there are no duplicates
+  arrange(name)
