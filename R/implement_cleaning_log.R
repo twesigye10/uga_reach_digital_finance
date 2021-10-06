e1c0c1fe075153677fa03a80f72436470a8aa0c4
Anthony Twesigye <twesigye10@gmail.com>
2021-10-06 08:25:47 +0300

    filtering out test data

--- R/implement_cleaning_log.R
+++ R/implement_cleaning_log.R
@@ -11,6 +11,24 @@ df_cleaning_log <- read_csv("inputs/combined_logic_spatial_and_others_checks.csv
   select(uuid, type, name, value, issue_id, sheet, index, relevant, issue)
 
 df_raw_data <- readxl::read_excel("inputs/UGA2103_Financial_Service_Providers_Assessment_HH_Tool_June2021.xlsx") %>% 
+  filter(consent == "yes", 
+         as_date(start) > as_date("2021-08-29"), 
+         point_number != "13m.", 
+         !as_date(start) %in% c(as_date("2021-09-08"), as_date("2021-09-09"), 
+                                as_date("2021-09-21")),
+         !str_detect(string = point_number, pattern = fixed('test', ignore_case = TRUE)),
+         !`_uuid` %in% c("27b0ffe2-8d47-4897-b402-1928fd23cfb3",
+                         "40d216de-76db-42b7-9105-aea1ce234489",
+                         "f2f648df-55d6-4b9d-93ca-aa87c3bc30c7",
+                         "4167b891-b1ff-46b1-856b-532dd28e7a1e",
+                         "d7bde578-cdc2-4d77-b31b-a15c4cec9d38",
+                         "4f3afa4f-a065-4f6d-bd25-9919902f9ce0",
+                         "a89d0010-d626-4a4f-8b8c-e83e8fade349",
+                         "27ac9f75-3954-4c55-90a3-b5b09984fe7a",
+                         "48ee8073-a0d7-460f-9867-304a47bdb1fc",
+                         "b0a1c83dcdb24671b9eed78d7a77786f",
+                         "c5c9b13aa6cd43338e113d8c647c04ed",
+                         "d8936d35-7e1c-48d9-bafa-b25e758c05eb" )) %>% 
   mutate(current_receive_cash = reduce2(.x = c("\\bSCI\\b", "\\bWTI\\b"), .y = c("sci", "wti"), .init = current_receive_cash, .f = str_replace),
          `current_receive_cash/sci` = ifelse(`current_receive_cash/SCI` == 1 & is.na(`current_receive_cash/sci`), `current_receive_cash/SCI`, `current_receive_cash/sci`),
          `current_receive_cash/wti` = ifelse(`current_receive_cash/WTI` == 1 & is.na(`current_receive_cash/wti`), `current_receive_cash/WTI`, `current_receive_cash/wti`)) %>% 
@@ -36,13 +54,13 @@ new_vars <- df_cleaning_log %>%
   rename(choice = value ) %>%
   select(name, choice) %>%
   filter(!paste0(name,"/",choice) %in% c("bank_acc_help_desk/bank_agent", 
-                          "card_feedback/yes_agents",
-                          "cash_feedback/yes_agents",
-                          "mm_feedback/police",
-                          "mm_feedback/yes_agents",
-                          "mm_limitations/conmen",
-                          "reason_want_cash_aid/other_sys_unsafe",
-                          "use_mm_acc_for/keep_money_safest")) %>% # ignore choice options removed from survey but still in the cleaning log
+                                         "card_feedback/yes_agents",
+                                         "cash_feedback/yes_agents",
+                                         "mm_feedback/police",
+                                         "mm_feedback/yes_agents",
+                                         "mm_limitations/conmen",
+                                         "reason_want_cash_aid/other_sys_unsafe",
+                                         "use_mm_acc_for/keep_money_safest")) %>% # ignore choice options removed from survey but still in the cleaning log
   distinct() %>% # to make sure there are no duplicates
   arrange(name)
 
@@ -67,7 +85,7 @@ new_vars_sm <- new_vars %>%
   left_join(df_survey_sm, by = "name") %>% 
   filter(q_type == "sm") %>% 
   mutate(new_cols = paste0(name,"/",choice))
-  
+
 # add new columns to the raw data -----------------------------------------
 
 df_raw_data_modified <- df_raw_data %>% 
