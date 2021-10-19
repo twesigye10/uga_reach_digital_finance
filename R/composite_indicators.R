cad37112fce9240c2a955f77f16fb10630344073
Anthony Twesigye <twesigye10@gmail.com>
2021-10-19 13:31:37 +0300

    changed the glueing function

--- R/composite_indicators.R
+++ R/composite_indicators.R
@@ -38,13 +38,13 @@ create_composite_indicators_dfa <- function(input_df) {
       i.refugee_settlement = ifelse(district_name == "adjumani" & status == "refugee", "adjumani", settlement_name),
       i.region = ifelse(district_name %in% c("isingiro", "kamwenge", "kikuube", "kyegegwa"), "South west", "West nile"),
       i.respondent_age = ifelse(respondent_age < 60, "age_btn_18-59", "age_greater_59"),
-      int.disability = glue::glue(vulnerability_see, vulnerability_hear, vulnerability_walk, vulnerability_concentrate, vulnerability_communicate, .sep = " "),
+      int.disability = paste(vulnerability_see, vulnerability_hear, vulnerability_walk, vulnerability_concentrate, vulnerability_communicate),
       i.disability = ifelse(str_detect(string = int.disability, pattern = "yes_a_lot_of_difficulty|cannot_do_at_all"), "yes_disability", "no_disability"),
       i.education_level = case_when(hh_member_education %in% c("complete_prof_degree", "complete_university") ~ "Higher",
                                     hh_member_education %in% c("incomplete_secondary", "complete_primary", "incomplete_primary") ~ "Low",
                                     hh_member_education %in% c("incomplete_prof_degree", "incomplete_university", "complete_voc_train", "complete_secondary", "incomplte_voc_train") ~ "Middle",
                                     hh_member_education %in% c("no_formal_educ") ~ "None",
-                                    TRUE ~ Other
+                                    TRUE ~ "Other"
       ),
       i.language_understand_number = str_count(string = language_understand, pattern = "[a-z]+.\\b"),
       i.phones_owned_hh = case_when(no_phones_hh_owns == 0 ~ "num_phones_0",
