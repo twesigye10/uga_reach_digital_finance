97365d9595b5071b3e09f0fe60581729034f8c1c
Anthony Twesigye <twesigye10@gmail.com>
2021-10-20 14:00:54 +0300

    loading population

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -14,6 +14,14 @@ df_cleaned <- read_csv("outputs/20211005_clean_data")
 dap <- read_csv("inputs/r_dap.csv") %>% 
   janitor::clean_names()
 
+start<- Sys.time() 
+
+# load in individual level population data sets
+df_ref_pop<- read_csv("inputs/refugee_population.csv")
+df_host_pop<- read_csv("inputs/host_population.csv")
+
+
 # make composite indicator ------------------------------------------------
 
-df_with_composites <- create_composite_indicators_dfa(input_df = df_cleaned)
+df_with_composites <- create_composite_indicators_dfa(input_df = df_cleaned) 
+
