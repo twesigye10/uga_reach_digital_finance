5e247b2144188dd1bea5627a78d0c5f1b646fa01
Anthony Twesigye <twesigye10@gmail.com>
2021-10-20 13:53:28 +0300

    read in the data and dap

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -6,3 +6,10 @@ library(glue)
 # source some scripts
 source("R/composite_indicators.R")
 source("R/make_weights.R")
+
+# load data ---------------------------------------------------------------
+
+df_cleaned <- read_csv("outputs/20211005_clean_data")
+
+dap <- read_csv("inputs/r_dap.csv") %>% 
+  janitor::clean_names()
