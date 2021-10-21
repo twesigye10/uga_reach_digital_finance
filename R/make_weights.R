462bc618ebf7e82f8b83560f3257647e16f78bc9
Anthony Twesigye <twesigye10@gmail.com>
2021-10-21 11:40:24 +0300

    weight for the host

--- R/make_weights.R
+++ R/make_weights.R
@@ -18,3 +18,22 @@ make_refugee_weight_table<- function(input_df_ref, input_refugee_pop){
            weights = (pop_strata/pop_global)/(sample_strata/sample_global)
     )
 }
+
+make_host_weight_table<- function(input_df_host, input_host_pop){
+  host_pop <- input_host_pop %>% 
+    mutate(strata = paste0(strata, "_host"),
+           pop_status= "host")
+  
+  host_weight_table <- input_df_host %>% 
+    group_by(strata) %>% 
+    summarise(
+      sample_strata=  n()
+    )%>% 
+    mutate(
+      sample_global= sum(sample_strata)) %>% 
+    left_join(host_pop) %>% 
+    rename(pop_strata= "total_pop") %>% 
+    mutate(pop_global=sum(pop_strata) ,
+           weights = (pop_strata/pop_global)/(sample_strata/sample_global)
+    )
+}
